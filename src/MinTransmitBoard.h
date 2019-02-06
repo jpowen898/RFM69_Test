e1636b5773eb7d26e1384caf36eccbda7cad7eb3
Parker <plowen12@gmail.com>
2019-02-05 18:47:50 -0700

    added an h file for the new board's pins

---
+++ src/MinTransmitBoard.h
@@ -0,0 +1,17 @@
+#ifndef MinTransmitBoard
+#define MinTransmiteBoard
+#include "mbed.h"
+//Board specific pinouts
+#define B1 D3
+#define B2 D8
+#define SERDEB P0_10
+#define RCLK D15
+#define SRCLK D14
+#define LED1 P0_1
+#define MOSI D11
+#define MISO D12
+#define SPICLK D13
+#define DIO0 D9
+#define NSS D10
+
+#endif
